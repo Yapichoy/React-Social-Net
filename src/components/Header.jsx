f6b56300fa760e132053833351a8e47ddc0a2529
Yapichoy <torezracer@gmail.com>
2021-03-02 16:23:22 +0300

    Lesson 12

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+  <header className="header">
+    <img src="https://mediaprocessor.websimages.com/fit/1920x1920/gutternation.webs.com/Gutter%20Logo2.png" alt=""/>
+  </header>
+);
+
+export default Header;
